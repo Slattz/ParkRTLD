0b929f5efc85ed7dfe33f21066e54b20166a4747
Slattz <Slattz@users.noreply.github.com>
2021-02-06 13:09:47 +0000

    Check for latest ACNH version first

--- source/main.cpp
+++ source/main.cpp
@@ -2,14 +2,18 @@
 #include "sdk_funcs.hpp"
 #include "svc.hpp"
 
-typedef void (*ACNH100_121)(uintptr_t, uintptr_t, void (*)(void), void (*)(void));
-typedef void (*ACNH130_170)(uintptr_t, uintptr_t, void (*)(void), void (*)(void), void (*)(void));
+typedef void (*ACNH100_121)(uintptr_t, uintptr_t, void (*)(void), void (*)(void)); //ACNH 1.0.0 - 1.2.1
+typedef void (*ACNH130_1XX)(uintptr_t, uintptr_t, void (*)(void), void (*)(void), void (*)(void)); //ACNH 1.3.0 - 1.7.0+
 
 extern "C" void __rtld_init(void) {
-    //svcOutputDebugString("__rtld_init called\n", 20);
+#ifdef DEBUG_EXECUTION
+    svcOutputDebugString("__rtld_init called\n", 20);
+#endif
 }
 extern "C" void __rtld_fini(void) {
-    //svcOutputDebugString("__rtld_fini called\n", 20);
+#ifdef DEBUG_EXECUTION
+    svcOutputDebugString("__rtld_fini called\n", 20);
+#endif
 }
 
 extern "C" void __rtld_call_start(uintptr_t mainThreadHandle, uintptr_t argAddr,
@@ -18,18 +22,18 @@ extern "C" void __rtld_call_start(uintptr_t mainThreadHandle, uintptr_t argAddr,
                 void (*call_fini_modules)()
             )
 {
-    if (static_cast<ACNH100_121>(nn::init::Start)) {
-        #ifdef DEBUG_EXECUTION
-        svcOutputDebugString("Using ACNH version 1.0.0 - 1.2.1!", 34);
-        #endif
-        nn::init::Start(mainThreadHandle, argAddr, call_enable_exception_handler, call_init_modules);
-    }
-
-    else if (static_cast<ACNH130_170>(nn::init::Start)) {
-        #ifdef DEBUG_EXECUTION
+    if (static_cast<ACNH130_1XX>(nn::init::Start)) { //Check latest version first rather than oldest version
+        #if defined(DEBUG_EXECUTION) || defined(PRINT_VERSION)
         svcOutputDebugString("Using ACNH version 1.3.0 - 1.7.0!", 34);
         #endif
         nn::init::Start(mainThreadHandle, argAddr, call_enable_exception_handler, call_init_modules, call_fini_modules);
+    }
+
+    else if (static_cast<ACNH100_121>(nn::init::Start)) {
+        #if defined(DEBUG_EXECUTION) || defined(PRINT_VERSION)
+        svcOutputDebugString("Using ACNH version 1.0.0 - 1.2.1!", 34);
+        #endif
+        nn::init::Start(mainThreadHandle, argAddr, call_enable_exception_handler, call_init_modules);
     }
 
     else {
